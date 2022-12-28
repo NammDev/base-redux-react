9dcdcaf8061e5f4150ec40267cfe30bd5283b49a
Nam <namdeveloper.ca@gmail.com>
2022-12-28 19:13:08 +0700

    Example useDispatch & useSelector

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,38 @@
+import { useSelector, useDispatch } from 'react-redux'
+
 function HomePage() {
+  const hobbyList = useSelector((state) => state.hobby.list)
+  const todoList = useSelector((state) => state.todo.list)
+
+  const dispatch = useDispatch()
+
+  const handleAdd = () => {
+    dispatch({
+      type: 'ADD_HOBBY',
+      payload: 'Coding',
+    })
+  }
+
   return (
     <div className='home-page'>
-      <h1>Home Page</h1>
+      <div className='hobby'>
+        <h3>Hobby List</h3>
+        <ul>
+          {hobbyList.map((list, i) => (
+            <li key={i}>{list}</li>
+          ))}
+        </ul>
+        <button onClick={handleAdd}>Add</button>
+      </div>
+
+      <div className='todo'>
+        <h3>Todo List</h3>
+        <ul>
+          {todoList.map((list, i) => (
+            <li key={i}>{list}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
